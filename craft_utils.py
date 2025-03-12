a02095cb317200ad0a8e4ae37e0a386078ca58bc
Won-Kyu Park <wkpark@gmail.com>
2025-03-12 23:26:48 +0900

    fix 'The requested array has an inhomogeneous shape' error when using --poly

--- craft_utils.py
+++ craft_utils.py
@@ -244,7 +244,6 @@ def getDetBoxes(textmap, linkmap, text_threshold, link_threshold, low_text, poly
 
 def adjustResultCoordinates(polys, ratio_w, ratio_h, ratio_net = 2):
     if len(polys) > 0:
-        polys = np.array(polys)
         for k in range(len(polys)):
             if polys[k] is not None:
                 polys[k] *= (ratio_w * ratio_net, ratio_h * ratio_net)
